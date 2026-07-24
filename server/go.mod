8520609dc68483ace6c89beb72356b58b08a3a62
Darkroom4364 <ro.peneder@gmail.com>
2026-07-24 19:01:31 +0200

    fix(server): anchor payload artifact access

--- server/go.mod
+++ server/go.mod
@@ -5,6 +5,7 @@ go 1.23.0
 toolchain go1.23.8
 
 require (
+	github.com/google/safeopen v0.0.0-20260327150837-43626d6f4685
 	github.com/google/uuid v1.6.0
 	github.com/gorilla/websocket v1.5.3
 	golang.org/x/net v0.39.0
---
+++ server/internal/handlers/api/payload/payload_dir_unix.go
@@ -0,0 +1,86 @@
+//go:build unix
+
+package payload
+
+import (
+	"errors"
+	"fmt"
+
+	"golang.org/x/sys/unix"
+)
+
+func ensurePayloadClassDirectory(payloadRoot, class string) error {
+	if !validPayloadDirectorySegment(class) {
+		return errors.New("invalid payload class directory")
+	}
+	rootFD, err := unix.Open(
+		payloadRoot,
+		unix.O_RDONLY|unix.O_DIRECTORY|unix.O_CLOEXEC,
+		0,
+	)
+	if err != nil {
+		return fmt.Errorf("open payload root: %w", err)
+	}
+	defer unix.Close(rootFD)
+
+	if err := unix.Mkdirat(rootFD, class, 0700); err != nil &&
+		!errors.Is(err, unix.EEXIST) {
+		return fmt.Errorf("create payload class directory: %w", err)
+	}
+	classFD, err := openPayloadDirectoryAt(rootFD, class)
+	if err != nil {
+		return fmt.Errorf("open payload class directory: %w", err)
+	}
+	defer unix.Close(classFD)
+	if err := unix.Fchmod(classFD, 0700); err != nil {
+		return fmt.Errorf("restrict payload class directory: %w", err)
+	}
+	return nil
+}
+
+func createPayloadBuildDirectory(
+	payloadRoot string,
+	class string,
+	buildID string,
+) error {
+	if !validPayloadDirectorySegment(class) ||
+		!validPayloadDirectorySegment(buildID) {
+		return errors.New("invalid payload build directory")
+	}
+	rootFD, err := unix.Open(
+		payloadRoot,
+		unix.O_RDONLY|unix.O_DIRECTORY|unix.O_CLOEXEC,
+		0,
+	)
+	if err != nil {
+		return fmt.Errorf("open payload root: %w", err)
+	}
+	defer unix.Close(rootFD)
+	classFD, err := openPayloadDirectoryAt(rootFD, class)
+	if err != nil {
+		return fmt.Errorf("open payload class directory: %w", err)
+	}
+	defer unix.Close(classFD)
+
+	if err := unix.Mkdirat(classFD, buildID, 0700); err != nil {
+		return fmt.Errorf("create payload build directory: %w", err)
+	}
+	buildFD, err := openPayloadDirectoryAt(classFD, buildID)
+	if err != nil {
+		return fmt.Errorf("open payload build directory: %w", err)
+	}
+	defer unix.Close(buildFD)
+	if err := unix.Fchmod(buildFD, 0700); err != nil {
+		return fmt.Errorf("restrict payload build directory: %w", err)
+	}
+	return nil
+}
+
+func openPayloadDirectoryAt(parentFD int, name string) (int, error) {
+	return unix.Openat(
+		parentFD,
+		name,
+		unix.O_RDONLY|unix.O_DIRECTORY|unix.O_NOFOLLOW|unix.O_CLOEXEC,
+		0,
+	)
+}
